78de0aee25d59d03cd25bbf9fdd0ca57038503ef
Luiz Corte Real <login+luiz@elo7.com>
2020-03-05 14:47:49 -0300

    [mocha-chrome] Atualiza dependências e script de teste

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+function finish {
+  ./tearDown.sh
+}
+
+trap finish EXIT
+
 ./setup.sh
 ./node_modules/.bin/mocha-chrome --reporter spec "http://localhost:8888/test/docTest.js.html";
-./tearDown.sh
